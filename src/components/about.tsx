d7e7df534d8b066b0d1da21b63bbb9bb5b743d54
Lucas Hagfoss <afs0028447@agderskolen.no>
2024-09-10 23:19:54 +0200

    not final release

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -8,7 +8,7 @@ export default function About() {
 
     return (
         <div className="absolute bottom-0 w-[100vw] flex justify-center items-center p-10">
-            <div className="text-[#1a1a1a] flex justify-center items-center text-[1vw]">
+            <div className="text-[#1a1a1a] flex justify-center items-center sm:text-[2.5vw] md:text-[2vw] lg:text-[1.5vw]">
                 <motion.div initial={{ y: 100 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 3.6, ease: easeInOut }} className="">I`m Lucas Hagfoss, a user experience & interface designer from Norway, focusing on building functional websites that will increase conversion and help reach customers.</motion.div>
             </div>
         </div>
