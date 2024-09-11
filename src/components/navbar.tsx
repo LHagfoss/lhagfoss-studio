df93b4eef1f98074164b4017ccc4a227e38594ed
Lucas Hagfoss <afs0028447@agderskolen.no>
2024-09-11 09:52:29 +0200

    test output

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,36 +5,38 @@ import { motion, easeInOut } from "framer-motion"
 import Link from "next/link";
 
 export default function Navbar() {
+    const [date, setDate] = useState(new Date().toLocaleDateString());
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleNavbar = () => {
         setIsOpen(!isOpen);
     };
 
+    const updateDate = () => {
+        setDate(new Date().toLocaleDateString());
+    };    
+
     return (
         <div className="navbar fixed w-[100vw] flex justify-center items-center p-10">
-            <div className="w-full font-bestFont text-[#1a1a1a] justify-between items-start leading-10 tracking-wide sm:text-[2.5vw] sm:hidden md:text-[2vw] md:flex lg:text-[1.5vw] lg:flex hidden">
+            <div className="w-full font-bestFont text-[#1a1a1a] justify-between items-start tracking-wide sm:text-[2.5vw] sm:hidden md:text-[2vw] md:flex lg:text-[1.5vw] lg:flex hidden">
                 <div className="flex flex-col">
                     <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 2, ease: easeInOut }} className=""><Link href="/">lucas</Link></motion.div>
                 </div>
                 <div className="flex flex-col">
                     <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 2.2, ease: easeInOut }} className=""><Link href="/">digital studio</Link></motion.div>
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col leading-10">
                     <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 2.4, ease: easeInOut }} className=""><Link href="/">work</Link></motion.div>
                     <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 2.6, ease: easeInOut }} className=""><Link href="/">about</Link></motion.div>
                     <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 2.8, ease: easeInOut }} className=""><Link href="/">other</Link></motion.div>
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col leading-10">
                     <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 3, ease: easeInOut }} className=""><Link href="/">github</Link></motion.div>
                     <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 3.2, ease: easeInOut }} className=""><Link href="/">contact</Link></motion.div>
                 </div>
-                <div className="flex flex-col gap-2">
-                    <div className="flex gap-5">
-                        <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 3.4, ease: easeInOut }} className=""><Link href="/signup" className="bg-[#1a1a1a] text-[#ebebeb] p-1 px-3 rounded-[5px]">Sign Up</Link></motion.div>
-                        <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 3.6, ease: easeInOut }} className=""><Link href="/signin" className="border border-[#1a1a1a] p-1 px-3 rounded-[5px]">Sign In</Link></motion.div>
-                    </div>
-                    <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 3.8, ease: easeInOut }} className=""><Link href="/">norway, krs</Link></motion.div>
+                <div className="flex gap-3">
+                    <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 3.6, ease: easeInOut }} className=""><Link href="/signup" className="bg-[#1a1a1a] text-[#ebebeb] p-2 px-4 rounded-[5px]">sign Up</Link></motion.div>
+                    <motion.div initial={{ y: -200 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 3.8, ease: easeInOut }} className=""><Link href="/signin" className="border border-[#1a1a1a] p-2 px-4 rounded-[5px]">sign in</Link></motion.div>
                 </div>
             </div>
             <div className="navbar w-full font-bestFont text-[#1a1a1a] sm:text-[2.5vw] sm:flex md:text-[2vw] md:hidden lg:text-[1.5vw] lg:hidden flex">
@@ -59,6 +61,11 @@ export default function Navbar() {
                         </div>
                     </div>
                 </motion.div>
+            </div>
+            <div className="fixed bottom-0 left-0 m-10">v.0.5</div>
+            <div className="fixed bottom-0 right-0 m-10">
+                <div className="">norway, krs</div>
+                <div className="">{date}</div>
             </div>
         </div>
     )
--- src/components/navn.tsx
+++ src/components/navn.tsx
@@ -9,13 +9,12 @@ export default function NavnAnimation() {
     return (
         <div className="w-[100vw] h-[100vh] flex justify-center items-center">
             <div className="font-bestFont text-[#1a1a1a] flex justify-center items-center text-[38vw] h-[30vw] overflow-hidden">
-                <motion.div initial={{ y: 700 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1, ease: easeInOut }} className="">l</motion.div>
-                <motion.div initial={{ y: 700 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1.2, ease: easeInOut }} className="">u</motion.div>
-                <motion.div initial={{ y: 700 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1.4, ease: easeInOut }} className="">c</motion.div>
-                <motion.div initial={{ y: 700 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1.6, ease: easeInOut }} className="">a</motion.div>
-                <motion.div initial={{ y: 700 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1.8, ease: easeInOut }} className="">s</motion.div>
+                <motion.div initial={{ y: 800 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1, ease: easeInOut }} className="">l</motion.div>
+                <motion.div initial={{ y: 800 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1.2, ease: easeInOut }} className="">u</motion.div>
+                <motion.div initial={{ y: 800 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1.4, ease: easeInOut }} className="">c</motion.div>
+                <motion.div initial={{ y: 800 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1.6, ease: easeInOut }} className="">a</motion.div>
+                <motion.div initial={{ y: 800 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1.8, ease: easeInOut }} className="">s</motion.div>
             </div>
-            <div className="fixed bottom-0 right-0 m-10">v.0.5</div>
         </div>
     )
 }
