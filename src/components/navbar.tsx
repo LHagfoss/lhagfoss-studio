71c6144f28a4b7b143628052f5a869cfd38f9729
Lucas Hagfoss <afs0028447@agderskolen.no>
2024-09-12 10:17:49 +0200

    test output

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState } from "react";
-import { motion, easeInOut, easeIn } from "framer-motion"
+import { motion, easeInOut } from "framer-motion"
 import Link from "next/link";
 
 export default function Navbar() {
