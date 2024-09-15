b65fa145dafac2271d346d391eb2ddbceb455772
Lucas Hagfoss <afs0028447@agderskolen.no>
2024-09-15 21:53:58 +0200

    gjemmestuff

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar() {
     const copyEmail = () => {
         navigator.clipboard.writeText("lucash.1707@gmail.com");
         setShowPopup(true);
-        setTimeout(() => setShowPopup(false), 1500);
+        setTimeout(() => setShowPopup(false), 2000);
     };
 
     return (
@@ -56,6 +56,9 @@ export default function Navbar() {
                     </div>
             </div>
             <div className="navbar w-full font-bestFont text-[#1a1a1a] sm:text-[2.5vw] sm:flex md:text-[2vw] md:hidden lg:text-[1.5vw] lg:hidden flex">
+                <div className="fixed top-0 left-0 z-10 w-full flex flex-row justify-start items-center p-5 text-[10vw]">
+                    <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 10 }}><MotionLink delay={2} href="/">lucas</MotionLink></motion.div>
+                </div>
                 <div className="fixed top-0 right-0 z-10 w-full flex flex-row justify-end items-center p-5">
                     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 2, ease: easeInOut }}><button type="button" onClick={toggleNavbar} className="toggle-button w-[70px] h-[70px] bg-[#1a1a1a] rounded-full text-[#ebebeb] cursor-pointer">{isOpen ? '=' : 'x'}</button></motion.div>
                 </div>
@@ -66,14 +69,21 @@ export default function Navbar() {
                     className="w-full h-full top-0 left-0 bg-secondary fixed p-[5vw] py-[30vw] font-bestFont text-[#1a1a1a] justify-around items-start overflow-hidden tracking-wide leading-20 text-[10vw] flex flex-col"
                 >
                     <div className="flex flex-col">
-                    <motion.div initial={{ x: -400 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 2, ease: easeInOut }}><Link href="/">lucas</Link></motion.div>
-                        <motion.div initial={{ x: -400 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 2.2, ease: easeInOut }}><Link href="/">digital studio</Link></motion.div>
-                        <motion.div initial={{ x: -400 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 2.4, ease: easeInOut }}><Link href="/">project / work</Link></motion.div>
-                        <motion.div initial={{ x: -400 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 2.6, ease: easeInOut }}><Link href="/">about</Link></motion.div>
-                        <motion.div initial={{ x: -400 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 2.8, ease: easeInOut }}><Link href="/">other</Link></motion.div>
-                        <motion.div initial={{ x: -400 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 3, ease: easeInOut }}><Link href="/">github</Link></motion.div>
-                        <motion.div initial={{ x: -400 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 3.2, ease: easeInOut }}><Link href="/">contact</Link></motion.div>
-                        <motion.div initial={{ y: -400 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 3.4, ease: easeInOut }}><div className="bg-primary text-secondary p-3 px-6 rounded-full flex items-center gap-3 border text-[4vw]"><Image src={copy} alt=""></Image><div className="">lucash.1707@gmail.com</div></div></motion.div>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 10 }}><MotionLink delay={2.2} href="/">Digital Studio</MotionLink></motion.div>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 10 }}><MotionLink delay={2.4} href="/project">project / work</MotionLink></motion.div>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 10 }}><MotionLink delay={2.6} href="/about">about / info</MotionLink></motion.div>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 10 }}><MotionLink delay={2.8} href="/other">other</MotionLink></motion.div>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 10 }}><MotionLink delay={3} href="/github">github</MotionLink></motion.div>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 10 }}><MotionLink delay={3.2} href="/contact">contact</MotionLink></motion.div>
+                        <motion.div initial={{ y: -400 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 3.4, ease: easeInOut }}>
+                            <div onClick={copyEmail} className="bg-primary text-secondary p-3 px-6 rounded-full flex items-center gap-3 border text-[4vw]">
+                                <Image src={copy} alt=""></Image>
+                                <div className="">lucash.1707@gmail.com</div>
+                                {showPopup && (
+                                <div className="absolute top-0 left-1/2 transform -translate-x-1/2 mt-20 bg-primary rounded-full text-white p-2 px-5 flex justify-center text-[5vw]">copied email</div>
+                            )}
+                            </div>
+                        </motion.div>
                     </div>
             </motion.div>
             </div>
