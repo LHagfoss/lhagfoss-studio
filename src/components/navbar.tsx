89c1b3d5b935fa88f93f0bc7fefb13ca239c43c4
lagos <lucash.1707@gmail.com>
2024-10-15 22:39:37 +0200

    small update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { motion, easeInOut } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
