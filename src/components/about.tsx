73411cb08c457148ea688417f8fff9efb1aa1040
Lucas Hagfoss <afs0028447@agderskolen.no>
2024-10-16 14:42:06 +0200

    not a major update

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -4,20 +4,28 @@ import { motion, easeInOut } from "framer-motion";
 export default function About() {
     
     const text = "I\'m Lucas Hagfoss, a user interface & user experience designer from Norway, focusing on building functional websites that will increase conversion and help reach customers.";
-    const text2 = "I specializing in React, Next.js, TypeScript, and other JavaScript frameworks. I have a strong understanding of various software and programs to develop a good website for UI \& UX.";
+    const text2 = "I specialize in React.js \& Next.js and other JavaScript frameworks. I use TypeScript and other useful tools and libraries to develop these websites. I have a strong understanding of various software and programs to develop a good website for UI \& UX.";
+    const text3 = "Balls Balls";
 
     return (
-        <div className="relative bottom-0 w-full md:w-[50vw] flex flex-col gap-5 items-start p-10 md:text-[1.5vw] md:leading-5 leading-3 text-[5vw]">
-            <div className="w-full text-thirdy flex flex-wrap overflow-hidden">
+        <div className="relative bottom-0 w-full md:w-[100vw] flex flex-col gap-3 items-start p-10 md:text-[1.5vw] leading-3 md:leading-6 text-[5vw]">
+            <div className="w-full text-thirdy flex flex-wrap overflow-hidden text-[3vw] leading-12">
                 {text.split(" ").map((word, index) => (
-                    <motion.div key={index} initial={{ y: 450 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1 + index * 0.05, ease: easeInOut }} className="p-2">{word}</motion.div>
+                    <motion.div key={index} initial={{ y: 450 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 1 + index * 0.05, ease: easeInOut }} className={`p-1 ${index === 1 || index === 2 ? 'text-secondary' : ''}`}>{word}</motion.div>
                 ))}
             </div>
-            <div className="bg-secondary h-full w-[2px]"></div>
-            <div className="w-full text-thirdy flex flex-wrap overflow-hidden">
-                {text2.split(" ").map((word, index) => (
-                    <motion.div key={index} initial={{ y: 450 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 2 + index * 0.05, ease: easeInOut }} className="p-2">{word}</motion.div>
-                ))}
+            <div className="bg-thirdy w-full h-[2px]"></div>
+            <div className="flex w-full text-[2vw]">
+                <div className="w-1/2 text-thirdy flex flex-wrap overflow-hidden">
+                    {text2.split(" ").map((word, index) => (
+                        <motion.div key={index} initial={{ y: 450 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 2 + index * 0.05, ease: easeInOut }} className="p-1">{word}</motion.div>
+                    ))}
+                </div>
+                <div className="w-1/2 text-thirdy flex flex-wrap overflow-hidden">
+                    {text3.split(" ").map((word, index) => (
+                        <motion.div key={index} initial={{ y: 450 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 2 + index * 0.05, ease: easeInOut }} className="p-1">{word}</motion.div>
+                    ))}
+                </div>
             </div>
         </div>
     );
