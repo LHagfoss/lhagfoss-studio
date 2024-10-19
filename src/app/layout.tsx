49690497dffa0b2d9fc6a0b2ab4861e57b585bf1
lagos <lucash.1707@gmail.com>
2024-10-20 00:29:39 +0200

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -41,6 +42,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleAnalytics trackPageViews />
       </body>
     </html>
   );
