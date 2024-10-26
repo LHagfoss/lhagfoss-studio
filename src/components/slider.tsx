d3100820488cca35e0f469f7c5a0e007c03f74f4
LHagfoss <afs0028447@agderskolen.no>
2024-10-27 00:19:41 +0200

    Fixed bugs in Grid.tsx, and updated Slider animation/transition

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { StaticImageData } from "next/image";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
+import FadeIn from "./fadein";
+
 import projectImage1 from "@/assets/googleblablalba.jpg"
 import projectImage2 from "@/assets/image97-transformed.jpeg"
 
@@ -60,46 +62,48 @@ export default function ImageSlider(): JSX.Element {
     };
   
     return (
-      <div className="relative w-full p-5 md:px-44">
-        <div className="w-full relative inset-0">
-            <div
-            className="relative h-[460px] mx-10 md:mx-32 group"
-            onMouseOver={handleMouseOver}
-            onMouseLeave={handleMouseLeave}
-            >
-            <Image
-                src={images[currentIndex].src}
-                alt={`Slider Image ${currentIndex + 1}`}
-                layout="fill"
-                objectFit="cover"
-                className="rounded-xl transition-all duration-500 ease-in-out cursor-pointer"
-            />
+        <FadeIn delay={0}>
+            <div className="relative w-full p-5 md:px-44">
+                <div className="w-full relative inset-0">
+                    <div
+                    className="relative h-[460px] mx-10 md:mx-32 group"
+                    onMouseOver={handleMouseOver}
+                    onMouseLeave={handleMouseLeave}
+                    >
+                    <Image
+                        src={images[currentIndex].src}
+                        alt={`Slider Image ${currentIndex + 1}`}
+                        layout="fill"
+                        objectFit="cover"
+                        className="rounded-xl transition-all duration-500 ease-in-out cursor-pointer"
+                    />
+                    </div>
+                    <button
+                    className="absolute left-0 top-1/2 transform h-[459px] rounded-xl hover:bg-secondary mx-1 -mt-[10px] duration-300 -translate-y-1/2 bg-primary text-white p-1 md:p-10 group"
+                    onClick={prevSlide}
+                    >
+                    <ChevronLeft className="text-secondary group-hover:text-fourth" />
+                    </button>
+                    <button
+                    className="absolute right-0 top-1/2 transform h-[459px] rounded-xl hover:bg-secondary mx-1 -mt-[10px] duration-300 -translate-y-1/2 bg-primary text-white p-1 md:p-10 group"
+                    onClick={nextSlide}
+                    >
+                    <ChevronRight className="text-secondary group-hover:text-fourth" />
+                    </button>
+                    <div className="flex justify-center mt-4">
+                    {images.map((_, index) => (
+                        <div
+                        key={index}
+                        className={`h-1 w-10 mx-1 ${
+                            index === currentIndex
+                            ? "bg-secondary rounded-xl"
+                            : "bg-primary rounded-xl"
+                        } transition-all duration-500 ease-in-out`}
+                        ></div>
+                    ))}
+                    </div>
+                </div>
             </div>
-            <button
-            className="absolute left-0 top-1/2 transform h-[459px] rounded-xl hover:bg-secondary mx-1 -mt-[10px] duration-300 -translate-y-1/2 bg-primary text-white p-1 md:p-10 group"
-            onClick={prevSlide}
-            >
-            <ChevronLeft className="text-secondary group-hover:text-fourth" />
-            </button>
-            <button
-            className="absolute right-0 top-1/2 transform h-[459px] rounded-xl hover:bg-secondary mx-1 -mt-[10px] duration-300 -translate-y-1/2 bg-primary text-white p-1 md:p-10 group"
-            onClick={nextSlide}
-            >
-            <ChevronRight className="text-secondary group-hover:text-fourth" />
-            </button>
-            <div className="flex justify-center mt-4">
-            {images.map((_, index) => (
-                <div
-                key={index}
-                className={`h-1 w-10 mx-1 ${
-                    index === currentIndex
-                    ? "bg-secondary rounded-xl"
-                    : "bg-primary rounded-xl"
-                } transition-all duration-500 ease-in-out`}
-                ></div>
-            ))}
-            </div>
-        </div>
-      </div>
+      </FadeIn>
     );
 }
