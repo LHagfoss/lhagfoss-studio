b73b8ee63e165937fe14b34e63bb8a31a693bbf7
LHagfoss <afs0028447@agderskolen.no>
2024-10-27 22:53:53 +0100

    Fixed bugs in Grid.tsx, and updated Slider animation/transition

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -8,6 +8,7 @@ import FadeIn from "./fadein";
 import norgeFlag from "@/assets/Flag_of_Norway.svg.png";
 import NextJSLogo from "@/assets/if82cpbS_400x400.jpg";
 import ReactJSLogo from "@/assets/react-logo-black-and-white-1.png";
+import info from "@/assets/info.svg"
 
 export default function Grid() {
     const [ref, inView] = useInView({
@@ -21,8 +22,8 @@ export default function Grid() {
                 {inView && (
                     <div className="p-5 md:p-44 w-[100vw] h-[100vw]">
                         <div className="w-full h-full grid grid-rows-4 grid-cols-4 gap-1 md:gap-5">
-                            <div className="bg-primary row-span-1 col-span-2 rounded-[2vw] p-5 overflow-hidden">
-                                <motion.div initial={{ y: 400 }} animate={{ y: 0 }} transition={{ delay: 1.6, duration: 1, ease: easeInOut }}  className="text-[#bebebe] text-[1.2vw] flex flex-col justify-between h-full">
+                            <div className="bg-primary row-span-1 col-span-2 rounded-[2vw] overflow-hidden">
+                                <motion.div initial={{ y: 400 }} animate={{ y: 0 }} transition={{ delay: 1.6, duration: 1, ease: easeInOut }}  className="text-[#bebebe] bg-[#655bff] text-[1.2vw] flex flex-col justify-between w-full p-5 h-full">
                                     <div className="flex w-full justify-between relative inset-0">
                                         <div className="flex flex-col leading-tight">
                                             <div className="text-[2.5vw] text-secondary">Lucas Hagfoss</div>
@@ -30,18 +31,42 @@ export default function Grid() {
                                         </div>
                                         <Image src={norgeFlag} alt="norge" className="absolute right-0 top-0 object-cover w-[8vw] h-[6vw] rounded-2xl"></Image>
                                     </div>
-                                    <div className="w-3/4">I&apos;m a UI & UX designer from Norway, focusing on building functional websites that will increase conversion and help reach customers.</div>
-                                    <div className="text-[1vw] text-thirdy">10/26/2024</div>
+                                    <div className="w-3/4">I&apos;m a Full-Stack developer from Norway, mainly focusing on UI & UX design. 🚀</div>
+                                    <div className="flex gap-3 items-center">
+                                        <div className="flex gap-1 justify-center items-center rounded-full border border-[#bebebe] p-1 px-2 cursor-pointer spring-duration-300 spring-bounce-80 hover:scale-105">
+                                            <Image src={info} alt="info" className=""></Image>
+                                            <div className="text-[#bebebe] text-[1vw]">More info</div>
+                                        </div>
+                                        <div className="text-[1vw] text-thirdy">10/26/2024</div>
+                                    </div>
                                 </motion.div>
                             </div>
-                            <div className="bg-primary row-span-2 col-span-1 rounded-[2vw]"></div>
-                            <div className="bg-primary row-span-1 col-span-1 rounded-[2vw] overflow-hidden hover:scale-105 spring-duration-300 spring-bounce-80 duration-300 hover:z-10">
-                                <motion.div initial={{ y: -400 }} animate={{ y: 0 }} transition={{ delay: 1.8, duration: 1, ease: easeInOut }} className="w-full h-full bg-[#655bff] p-2 md:p-5 flex flex-col justify-between rounded-[2vw]">
+                            <div className="bg-primary row-span-2 col-span-1 rounded-[2vw] overflow-hidden">
+                                <div className="w-full h-full rounded-[2vw] p-5 flex flex-col justify-between">
                                     <div className="flex flex-col">
-                                        <div className="text-secondary text-[3vw]">Test Title 23</div>
-                                        <div className="text-[#bebebe] text-[1.5vw] leading-[10px] md:leading-[2vw]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, ratione.</div>
+                                        <div className="text-secondary text-[2vw]">Happy Customers</div>
+                                        <div className="text-thirdy">(Not real customers, just for testing purpose)</div>
                                     </div>
-                                    <div className="text-[#bebebe] text-[1.5vw]">10/25/2024</div>
+                                    <div className="w-full flex flex-col gap-3">
+                                        <motion.div initial={{ x: -300 }} animate={{ x: 0 }} transition={{ delay: 1.3, duration: 1, ease: easeInOut }} className="w-full h-[150px] bg-fourth rounded-[1vw]">
+                                            <div className="">
+
+                                            </div>
+                                            <div className="">
+                                                
+                                            </div>
+                                            <div className="">
+                                                
+                                            </div>
+                                        </motion.div>
+                                        <motion.div initial={{ x: 300 }} animate={{ x: 0 }} transition={{ delay: 1.4, duration: 1, ease: easeInOut }} className="w-full h-[150px] bg-fourth rounded-[1vw]"></motion.div>
+                                        <motion.div initial={{ x: -300 }} animate={{ x: 0 }} transition={{ delay: 1.5, duration: 1, ease: easeInOut }} className="w-full h-[150px] bg-fourth rounded-[1vw]"></motion.div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="bg-primary row-span-1 col-span-1 rounded-[2vw] overflow-hidden">
+                                <motion.div initial={{ x: 400 }} animate={{ x: 0 }} transition={{ delay: 2.2, duration: 1, ease: easeInOut }} className="w-full h-full bg-[#171515] flex justify-center items-center rounded-[2vw] relative inset-0">
+                                    <Image src={ReactJSLogo} alt="nextjs image" className="w-full h-full object-cover"></Image>
                                 </motion.div>
                             </div>
                             <div className="bg-primary row-span-2 col-span-1 rounded-[2vw]"></div>
@@ -51,8 +76,15 @@ export default function Grid() {
                                 </motion.div>
                             </div>
                             <div className="bg-primary row-span-1 col-span-1 rounded-[2vw] overflow-hidden">
-                                <motion.div initial={{ x: 400 }} animate={{ x: 0 }} transition={{ delay: 2.2, duration: 1, ease: easeInOut }} className="w-full h-full bg-[#171515] flex justify-center items-center rounded-[2vw] relative inset-0">
-                                    <Image src={ReactJSLogo} alt="nextjs image" className="w-full h-full object-cover"></Image>
+                                <motion.div initial={{ y: -400 }} animate={{ y: 0 }} transition={{ delay: 1.8, duration: 1, ease: easeInOut }} className="w-full h-full bg-[#655bff] p-2 md:p-5 flex flex-col justify-between rounded-[2vw] ">
+                                    <div className="flex flex-col">
+                                        <div className="text-secondary text-[2.5vw]">Skills</div>
+                                        <div className="text-[#bebebe] text-[1vw] leading-[10px] md:leading-[1.5vw]">I&apos;m skilled with tools and frameworks like Next.js for website development, Figma for design, and various other technologies to ensure our customers are thrilled with the results.</div>
+                                    </div>
+                                    <div className="flex gap-1 justify-center items-center rounded-full border border-[#bebebe] p-1 px-2 cursor-pointer spring-duration-300 spring-bounce-80 hover:scale-105">
+                                        <Image src={info} alt="info" className=""></Image>
+                                        <div className="text-[#bebebe] text-[1vw]">More info</div>
+                                    </div>
                                 </motion.div>
                             </div>
                             <div className="bg-primary row-span-1 col-span-3 rounded-[2vw]"></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -117,6 +117,6 @@ module.exports = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate"), require('tailwindcss-spring'),],
+  plugins: [require("tailwindcss-animate"), require('tailwindcss-spring'), require('tailwind-scrollbar'),],
 }
 
